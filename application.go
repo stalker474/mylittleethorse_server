2e6a51ca3d9c3070f3f1edfb95d72d0f9c63e9ee
stalker474 <anton.possylkine@gmail.com>
2018-08-23 16:00:45 +0200

    CORS2

--- application.go
+++ application.go
@@ -54,6 +54,8 @@ func main() {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func updateCache() {
