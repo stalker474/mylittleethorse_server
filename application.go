b2d2b0e859e687327b34a25c04a446600d84fcc7
stalker474 <anton.possylkine@gmail.com>
2018-09-14 10:54:18 +0200

    a1

--- application.go
+++ application.go
@@ -437,19 +437,19 @@ func updateRaceData024(race *RaceData) error {
 		log.Println("#", race.RaceNumber, " Error deposits: ", err)
 		deposits, err = contract.Betting024Filterer.FilterDeposit(&bind.FilterOpts{Start: 5000000, End: nil, Context: nil})
 	}
-	defer deposits.Close()
+	//defer deposits.Close()
 	withdraws, err := contract.Betting024Filterer.FilterWithdraw(&bind.FilterOpts{Start: 5000000, End: nil, Context: nil})
 	for err != nil {
 		log.Println("#", race.RaceNumber, " Error withdraws: ", err)
 		withdraws, err = contract.Betting024Filterer.FilterWithdraw(&bind.FilterOpts{Start: 5000000, End: nil, Context: nil})
 	}
-	defer withdraws.Close()
+	//defer withdraws.Close()
 	refunds, err := contract.Betting024Filterer.FilterRefundEnabled(&bind.FilterOpts{Start: 5000000, End: nil, Context: nil})
 	for err != nil {
 		log.Println("#", race.RaceNumber, " Error refunds: ", err)
 		refunds, err = contract.Betting024Filterer.FilterRefundEnabled(&bind.FilterOpts{Start: 5000000, End: nil, Context: nil})
 	}
-	defer refunds.Close()
+	//defer refunds.Close()
 
 	if btcWon || ltcWon || ethWon {
 		race.WinnerHorses = nil
